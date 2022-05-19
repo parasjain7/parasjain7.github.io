8a400f73025f7141a416b37f873e34d93b856273
Paras Jain <jainparas8461@gmail.com>
2022-05-19 12:45:27 +0530

    back

--- src/context.js
+++ src/context.js
@@ -13,7 +13,7 @@ const themeReducer = (state, action)=>{
     }
 }
 
-export default function ThemeProvider(props){
+export const ThemeProvider = (props)=>{
     const [state, dispatch] = useReducer(themeReducer, INITIAL_STATE)
 
     return (
